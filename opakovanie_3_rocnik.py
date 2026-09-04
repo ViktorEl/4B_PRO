e6b65f8953df2e9f9dd4fcc3b259c816a2080cca
viktor <www.pista43@centrum.sk>
2026-09-04 10:25:56 +0200

    vyriesena uloha

--- opakovanie_3_rocnik.py
+++ opakovanie_3_rocnik.py
@@ -1,1 +1,25 @@
 # Prvé úlohy na preriesenie
+#Vytvorte funkciu pokuta(), 
+# ktorá ako vstupný parameter príjme počet dní omeškania 
+# faktúry a vráti (return) výšku poplatku/pokuty. 
+
+#Cenník pokút:
+#Omeškanie 0 dní 			0 eur
+#Omeškanie menej ako 6 dní	5 eur
+#Omeškanie menej ako 15 dní 	10 eur
+#Omeškanie 15 a viac dní		20 eur pokuta + upomienka
+
+def pokuta(pocet_dni):
+    if pocet_dni == 0:
+        return 0
+    elif pocet_dni < 6:
+        return 5
+    elif pocet_dni < 15:
+        return 10
+    else:
+        return '20 eur pokuta + upomienka'
+
+pocet_dni = int(input('Zadajte pocet dni:')) # pozor input vracia string
+#pocet_dni = int(pocet_dni) toto je jedna moznost v pretypovani
+
+print(pokuta(pocet_dni))
